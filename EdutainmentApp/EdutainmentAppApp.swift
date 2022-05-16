6b6131171c4cf4cf84990b84820d285d0eacd528
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-05-16 23:12:49 +0300

    Initial Commit

---
+++ EdutainmentApp/EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppApp.swift
+//  EdutainmentApp
+//
+//  Created by Alex Po on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
